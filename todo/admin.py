3b395dfd1e9a245e2bdc3756a98cc0df80f9cd57
Kajendiran M <64881916+kajendiranm@users.noreply.github.com>
2023-10-29 10:51:50 +0530

    Delete todo directory

--- todo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ProfileModel, TaskModel, FeedbackModel
-# Register your models here.
-
-admin.site.register(ProfileModel)
-admin.site.register(TaskModel)
-admin.site.register(FeedbackModel)
