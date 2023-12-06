35f684f064640b778a8bff2e3102fd14e7c3161f
Kajendiran M <64881916+kajendiranm@users.noreply.github.com>
2023-12-06 15:26:59 +0530

    Delete ajaxapp directory

--- ajaxapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class NameModel(models.Model):
-    user_name = models.CharField(max_length=15,unique=True)
-
-    def __str__(self):
-        return self.user_name
--- ajaxapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.home),
-    path("add",views.add)
-    #add /add while production if main url with "add" withour /
-]
